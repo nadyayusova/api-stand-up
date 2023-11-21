212d5a39dadeb113bc820b25264d419a6b0d88d1
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2023-11-21 23:00:15 +0300

    add day1 code

---
+++ index.js
@@ -0,0 +1,28 @@
+import http from 'node:http';
+import fs from 'node:fs/promises';
+
+const PORT = 8080;
+
+http
+  .createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url === '/comedians') {
+      try {
+        const data = await fs.readFile('comedians.json', 'utf-8');
+
+        res.writeHead(200, {
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        });
+        res.end(data);
+      } catch (error) {
+        res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
+        res.end(`Ошибка сервера: ${error}`);
+      }
+    } else {
+      res.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
+      res.end('Not Found');
+    }
+  })
+  .listen(PORT);
+
+console.log(`Сервер запущен по адресу http://localhost:${PORT}`);
